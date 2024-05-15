3aa8e80048969928a945c6603559fbc256ee20b6
RGC02995 <raulgc2995@gmail.com>
2024-05-15 21:43:38 +0200

    create a table

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -1,13 +1,16 @@
-/**
- *
- * @param {Number} page
- * @returns
- */
+//mappers
+import { localhostUserToModel } from "../mappers/localhost-user.mapper";
 
+/**
+ * @param {Number} page
+ * @returns {Promise<Users[]>}
+ */
 export const loadUsersByPage = async (page = 1) => {
   const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
   const res = await fetch(url);
   const data = await res.json();
 
-  console.log(data.data);
+  const users = data.data.map((userLike) => localhostUserToModel(userLike));
+
+  return users;
 };
